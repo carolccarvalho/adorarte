790648fea2edca0993b5a6cc1256996cf2ba5014
Ana Carolina <carolccarvalho@gmail.com>
2019-10-23 17:57:50 -0300

    Primeiro commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+import SlideNav from './modules/slide.js';
+
+const slide = new SlideNav('.slide', '.slide-wrapper');
+slide.init();
+slide.addControl('.custom-controls');
+
+function initScrollSuave() {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+    section.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener('click', scrollToSection);
+  });
+}
+initScrollSuave();
